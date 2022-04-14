80aa2bd00875c892ee743efd900cf7c72c811f19
25b3nk <csbhaskar95@gmail.com>
2022-04-14 21:23:29 +0530

    Added current repo as module

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module rest-api
+module github.com/25b3nk/learn-gomux
 
 go 1.16
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/lib/pq v1.10.4 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.5 // indirect
 )
--- main_test.go
+++ main_test.go
@@ -17,7 +17,7 @@ func TestMain(m *testing.M) {
 	ensureTableExists()
 	code := m.Run()
 	clearTable()
-	os.exit(code)
+	os.Exit(code)
 }
 
 func ensureTableExists() {
@@ -25,3 +25,16 @@ func ensureTableExists() {
 		log.Fatal(err)
 	}
 }
+
+func clearTable() {
+	a.DB.Exec("DELETE FROM products")
+	a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
+}
+
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
+(
+    id SERIAL,
+    name TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
+)`
